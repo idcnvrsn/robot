d1da2bc8c0e544389aeb23db0095e0091e7752bd
idcnvrsn <idcnvrsn@gmail.com>
2020-01-28 23:07:28 +0900

    rqt_graph、rqt_console、rqt_logger_levelを表示するようにした。 rosnode listを表示するようにした。

--- ros/osrf_melodic_turtlebot3/turtlebot3_nav.sh
+++ ros/osrf_melodic_turtlebot3/turtlebot3_nav.sh
@@ -1,3 +1,11 @@
 xterm -e roslaunch turtlebot3_gazebo turtlebot3_world.launch &
-sleep 5
+sleep 10
 xterm -e roslaunch turtlebot3_navigation turtlebot3_navigation.launch &
+
+rqt_graph &
+rqt_console &
+rqt_logger_level &
+
+echo 'rosnode list:'
+rosnode list
+
