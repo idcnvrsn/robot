84a75dc066081f1e1666e6f9b926c0e7d567ad76
idcnvrsn <idcnvrsn@gmail.com>
2020-01-28 21:42:13 +0900

    rosnode list実行。メッセージの内容表示。

--- ros/osrf_melodic_turtlebot3/turtlesim.sh
+++ ros/osrf_melodic_turtlebot3/turtlesim.sh
@@ -1,12 +1,18 @@
 roscore &
-sleep 3
+sleep 10
 #xterm -e rosrun turtlesim turtlesim_node &
 xterm -e rosrun turtlesim turtlesim_node __name:=my_turtle &
 
 xterm -e rosrun turtlesim turtle_teleop_key &
 xterm -e rostopic echo /turtle1/cmd_vel __name:=echo_tutle1_cmd_vel &
 xterm -e rqt_graph &
-sleep 5
 
-rostopic type /turtle1/cmd_vel | rosmsg show &
-
+sleep 10
+echo 'rosnode list:'
+rosnode list
+echo 'rostopic type /turtle1/cmd_vel | rosmsg show:'
+rostopic type /turtle1/cmd_vel | rosmsg show
+echo 'rostopic type /turtle1/pose | rosmsg show:'
+rostopic type /turtle1/pose | rosmsg show
+echo 'rosservice list:'
+rosservice list
