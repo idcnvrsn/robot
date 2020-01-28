98731f0b76aa98bd083574e18a51c7c05fb0e537
idcnvrsn <idcnvrsn@gmail.com>
2020-01-28 22:01:58 +0900

    rqt_console,rqt_logger_level を追加

--- ros/osrf_melodic_turtlebot3/turtlesim.sh
+++ ros/osrf_melodic_turtlebot3/turtlesim.sh
@@ -1,11 +1,15 @@
 roscore &
 sleep 10
+
+
 #xterm -e rosrun turtlesim turtlesim_node &
 xterm -e rosrun turtlesim turtlesim_node __name:=my_turtle &
 
 xterm -e rosrun turtlesim turtle_teleop_key &
 xterm -e rostopic echo /turtle1/cmd_vel __name:=echo_tutle1_cmd_vel &
-xterm -e rqt_graph &
+rosrun rqt_graph rqt_graph &
+rosrun rqt_console rqt_console &
+rosrun rqt_logger_level rqt_logger_level &
 
 sleep 10
 echo 'rosnode list:'
